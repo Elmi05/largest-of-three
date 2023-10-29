771569c70a606d774fdf01987d0b74d6d1389996
Elmi05 <karshe0548@gmail.com>
2023-10-29 15:04:37 +0300

    done assignment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,26 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+const num1 = 10;
+const num2 = 25;
+const num3 = 15;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(`The largest number out of the three numbers is ${num1}`);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(`The largest number out of the three numbers is ${num2}`);
+} else if (num3 > num1 && num3 > num2) {
+  console.log(`The largest number out of the three numbers is ${num3}`);
+}else {
+    console.log("The three numbers are equal");
+  }
+
+
+
+
+
+
+
+
